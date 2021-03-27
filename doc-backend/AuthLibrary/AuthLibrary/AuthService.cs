2cbf95709790e42335faa5fb43b82e9aeed15096
Arturo Cordoba <arturocv16@gmail.com>
2021-03-26 22:20:00 -0600

    Token generation & validation using JWT

---
+++ doc-backend/AuthLibrary/AuthLibrary/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthLibrary
+{
+    class AuthService: IAuthService
+    {
+        private string secretKey;
+        private string issuerToken;
+        private int expirationTime;
+
+        public AuthService(string secretKey, string issuerToken, int expirationTime = 0)
+        {
+            this.secretKey = secretKey;
+            this.issuerToken = issuerToken;
+            this.expirationTime = expirationTime;
+        }
+
+        public ITokenGenerator TokenGenerator
+        {
+            get
+            {
+                return new TokenGenerator(secretKey, issuerToken, expirationTime);
+            }
+        }
+
+        public ITokenValidator TokenValidator
+        {
+            get
+            {
+                return new TokenValidator(secretKey, issuerToken);
+            }
+        }
+    }
+}
---
+++ doc-backend/AuthLibrary/AuthLibrary/Token/TokenValidator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace AuthLibrary
+{
+    class TokenValidator : ITokenValidator
+    {
+        private string secretKey;
+        private string issuerToken;
+
+        public TokenValidator(string secretKey, string issuerToken)
+        {
+            this.secretKey = secretKey;
+            this.issuerToken = issuerToken;
+        }
+
+        /// <summary>
+        /// This method verify an encrypted token
+        /// </summary>
+        /// <param name="token">Encrypted token</param>
+        /// <returns>true if the token is valid, false otherwise</returns>
+        public bool VerifyToken(string token)
+        {
+            try
+            {
+                SecurityToken validatedToken;
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var validationParameters = TokenValidationParameters();
+
+                // This method throws an exception if the token is invalid
+                tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+
+                return true;
+            }
+            catch (SecurityTokenValidationException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// This method encapsulates the construction of the TokenValidationParameters object,
+        /// needed to validate the token
+        /// </summary>
+        /// <returns>Validation parameters object</returns>
+        private TokenValidationParameters TokenValidationParameters()
+        {
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
+
+            return new TokenValidationParameters()
+            {
+                ValidateAudience = false,
+                ValidateIssuer = true,
+                ValidIssuer = issuerToken,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+                ValidateLifetime = true,
+                LifetimeValidator = this.LifetimeValidator,
+            };
+        }
+
+        /// <summary>
+        /// This method checks if the token has expired
+        /// </summary>
+        /// <param name="notBefore"></param>
+        /// <param name="expires"></param>
+        /// <param name="securityToken"></param>
+        /// <param name="validationParameters"></param>
+        /// <returns></returns>
+        private bool LifetimeValidator(DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            if (expires != null)
+            {
+                if (DateTime.UtcNow < expires) return true;
+            }
+            return false;
+        }
+    }
+}
